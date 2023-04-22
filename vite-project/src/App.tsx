914fba9e0ade77ae51cfca61db3ef022b785aab2
Chandradeep Chowdhury <chandradeep.choudhury@gmail.com>
2023-04-22 12:24:47 -0700

    added recommendation column

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,22 +1,28 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import "./App.css";
-import DeviceAndBudgetForm from "./DeviceAndBudget";
-import UseCasesForm from "./UseCases";
-import SpecialForm from "./Special";
-import OptionalFilterForm from "./OptionalFilters";
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import './App.css';
+import DeviceAndBudgetForm from './DeviceAndBudget';
+import UseCasesForm from './UseCases';
+import SpecialForm from './Special';
+import OptionalFilterForm from './OptionalFilters';
+import RecommendationForm from './Recommendations';
 
 function App() {
-  const [count, setCount] = useState(0);
+    const [count, setCount] = useState(0);
 
-  return (
-    <div className="forms-container">
-      <DeviceAndBudgetForm />
-      <UseCasesForm />
-      <SpecialForm />
-      <OptionalFilterForm />
-    </div>
-  );
+    return (
+        <div className="forms-container">
+            <div className="left-column">
+                <DeviceAndBudgetForm />
+                <UseCasesForm />
+                <SpecialForm />
+                <OptionalFilterForm />
+            </div>
+            <div className="right-column">
+                <RecommendationForm />
+            </div>
+        </div>
+    );
 }
 
 export default App;
---
+++ vite-project/src/Recommendations.tsx
@@ -0,0 +1,49 @@
+import React, { useState, useEffect } from 'react';
+
+interface Monitor {
+    id: number;
+    name: string;
+    resolution: string;
+    refreshRate: number;
+    panel: string;
+    size: number;
+    cost: number;
+    minGpu: string;
+    specialFeatures: string;
+    curve: boolean;
+    aspectRatio: string;
+    reviews: string[];
+}
+
+const RecommendationForm: React.FC = () => {
+    const [monitors, setMonitors] = useState<Monitor[]>([]);
+
+    useEffect(() => {
+        fetch('/api/monitor-recommendations')
+            .then((res) => res.json())
+            .then((data) => setMonitors(data));
+    }, []);
+
+    return (
+        <div>
+            <h2>Monitor Recommendations</h2>
+            {monitors.map((monitor) => (
+                <div key={monitor.id}>
+                    <h3>{monitor.name}</h3>
+                    <p>Resolution: {monitor.resolution}</p>
+                    <p>Refresh Rate: {monitor.refreshRate} Hz</p>
+                    <p>Panel Type: {monitor.panel}</p>
+                    <p>Size: {monitor.size}"</p>
+                    <p>Cost: ${monitor.cost.toFixed(2)}</p>
+                    <p>Minimum GPU: {monitor.minGpu}</p>
+                    <p>Special Features: {monitor.specialFeatures}</p>
+                    <p>Curved: {monitor.curve ? 'Yes' : 'No'}</p>
+                    <p>Aspect Ratio: {monitor.aspectRatio}</p>
+                    <p>Reviews: {monitor.reviews.join(', ')}</p>
+                </div>
+            ))}
+        </div>
+    );
+};
+
+export default RecommendationForm;
