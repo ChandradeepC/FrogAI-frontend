6f43ab84a816cd47d934fcfa0a37641819a8456f
Chandradeep Chowdhury <chandradeep.choudhury@gmail.com>
2023-04-21 21:01:42 -0700

    refactored app

--- vite-project/src/main.tsx
+++ vite-project/src/main.tsx
@@ -2,18 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import DeviceAndBudgetForm from "./DeviceAndBudget";
-import UseCasesForm from "./UseCases";
-import SpecialForm from "./Special";
-import OptionalFilterForm from "./OptionalFilters";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <div className="forms-container">
-      <DeviceAndBudgetForm />
-      <UseCasesForm />
-      <SpecialForm />
-      <OptionalFilterForm />
+    <div>
+      <App />
     </div>
   </React.StrictMode>
 );
