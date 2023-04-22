d87b42f970fba4311d919120fe1b3744e1b10f25
Chandradeep Chowdhury <chandradeep.choudhury@gmail.com>
2023-04-21 21:32:40 -0700

    configured prettier

--- vite-project/src/OptionalFilters.tsx
+++ vite-project/src/OptionalFilters.tsx
@@ -1,145 +1,155 @@
-import React, { useState } from "react";
-import "./Component.css";
+import React, { useState } from 'react';
+import './Component.css';
 
 interface Props {}
 
 const OptionalFilterForm: React.FC<Props> = () => {
-  const [aspect, setAspect] = useState<string>("nopref");
-  const [curve, setCurve] = useState<string>("nopref");
-  const [size, setSize] = useState<string>("nopref");
-  const [res, setRes] = useState<string>("nopref");
-  const [minRR, setMinRR] = useState<string>("nopref");
-  const [panel, setPanel] = useState<string>("nopref");
-  const [backlight, setBacklight] = useState<string>("nopref");
+    const [aspect, setAspect] = useState<string>('nopref');
+    const [curve, setCurve] = useState<string>('nopref');
+    const [size, setSize] = useState<string>('nopref');
+    const [res, setRes] = useState<string>('nopref');
+    const [minRR, setMinRR] = useState<string>('nopref');
+    const [panel, setPanel] = useState<string>('nopref');
+    const [backlight, setBacklight] = useState<string>('nopref');
 
-  const handleInputChange = (
-    event: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
-  ) => {
-    const { name, value } = event.target;
-    switch (name) {
-      case "aspect":
-        setAspect(value);
-        break;
-      case "curve":
-        setCurve(value);
-        break;
-      case "size":
-        setSize(value);
-        break;
-      case "res":
-        setRes(value);
-        break;
-      case "minRR":
-        setMinRR(value);
-        break;
-      case "panel":
-        setPanel(value);
-        break;
-      case "backlight":
-        setBacklight(value);
-        break;
-      default:
-        break;
-    }
-  };
+    const handleInputChange = (
+        event: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
+    ) => {
+        const { name, value } = event.target;
+        switch (name) {
+            case 'aspect':
+                setAspect(value);
+                break;
+            case 'curve':
+                setCurve(value);
+                break;
+            case 'size':
+                setSize(value);
+                break;
+            case 'res':
+                setRes(value);
+                break;
+            case 'minRR':
+                setMinRR(value);
+                break;
+            case 'panel':
+                setPanel(value);
+                break;
+            case 'backlight':
+                setBacklight(value);
+                break;
+            default:
+                break;
+        }
+    };
 
-  return (
-    <div className="form-container">
-      <div>
-        <label htmlFor="aspect">Aspect:</label>
-        <select
-          id="aspect"
-          name="aspect"
-          value={aspect}
-          onChange={handleInputChange}
-        >
-          <option value="nopref">No preference</option>
-          <option value="wide">Wide</option>
-          <option value="ultrawide">Ultra Wide</option>
-          <option value="superultrawide">Super Ultra Wide</option>
-        </select>
-      </div>
-      <div>
-        <label htmlFor="curve">Curve:</label>
-        <select
-          id="curve"
-          name="curve"
-          value={curve}
-          onChange={handleInputChange}
-        >
-          <option value="nopref">No preference</option>
-          <option value="no">No</option>
-          <option value="yes">Yes</option>
-        </select>
-      </div>
-      <div>
-        <label htmlFor="size">Size:</label>
-        <select id="size" name="size" value={size} onChange={handleInputChange}>
-          <option value="nopref">No Preference</option>
-          <option value="24">24"</option>
-          <option value="25">25"</option>
-          <option value="27">27"</option>
-          <option value="32">32"</option>
-          <option value="34">34"</option>
-          <option value="38">38"</option>
-          <option value="49">49"</option>
-        </select>
-      </div>
-      {size === "49" && (
-        <div>
-          <label htmlFor="res">Resolution:</label>
-          <select id="res" name="res" value={res} onChange={handleInputChange}>
-            <option value="nopref">No preference</option>
-            <option value="5120x1440">5120x1440</option>
-            <option value="3840x1080">3840x1080</option>
-          </select>
+    return (
+        <div className="form-container">
+            <div>
+                <label htmlFor="aspect">Aspect:</label>
+                <select
+                    id="aspect"
+                    name="aspect"
+                    value={aspect}
+                    onChange={handleInputChange}
+                >
+                    <option value="nopref">No preference</option>
+                    <option value="wide">Wide</option>
+                    <option value="ultrawide">Ultra Wide</option>
+                    <option value="superultrawide">Super Ultra Wide</option>
+                </select>
+            </div>
+            <div>
+                <label htmlFor="curve">Curve:</label>
+                <select
+                    id="curve"
+                    name="curve"
+                    value={curve}
+                    onChange={handleInputChange}
+                >
+                    <option value="nopref">No preference</option>
+                    <option value="no">No</option>
+                    <option value="yes">Yes</option>
+                </select>
+            </div>
+            <div>
+                <label htmlFor="size">Size:</label>
+                <select
+                    id="size"
+                    name="size"
+                    value={size}
+                    onChange={handleInputChange}
+                >
+                    <option value="nopref">No Preference</option>
+                    <option value="24">24"</option>
+                    <option value="25">25"</option>
+                    <option value="27">27"</option>
+                    <option value="32">32"</option>
+                    <option value="34">34"</option>
+                    <option value="38">38"</option>
+                    <option value="49">49"</option>
+                </select>
+            </div>
+            {size === '49' && (
+                <div>
+                    <label htmlFor="res">Resolution:</label>
+                    <select
+                        id="res"
+                        name="res"
+                        value={res}
+                        onChange={handleInputChange}
+                    >
+                        <option value="nopref">No preference</option>
+                        <option value="5120x1440">5120x1440</option>
+                        <option value="3840x1080">3840x1080</option>
+                    </select>
+                </div>
+            )}
+            <div>
+                <label htmlFor="minRR">Minimum Refresh Rate:</label>
+                <select
+                    id="minRR"
+                    name="minRR"
+                    value={minRR}
+                    onChange={handleInputChange}
+                >
+                    <option value="nopref">No preference</option>
+                    <option value="60hz">60Hz</option>
+                    <option value="75hz">75Hz</option>
+                    <option value="120hz">120Hz</option>
+                    <option value="144hz">144Hz</option>
+                    <option value="240hz">240Hz</option>
+                </select>
+            </div>
+            <div>
+                <label htmlFor="panel">Panel Type:</label>
+                <select
+                    id="panel"
+                    name="panel"
+                    value={panel}
+                    onChange={handleInputChange}
+                >
+                    <option value="nopref">No preference</option>
+                    <option value="ips">IPS</option>
+                    <option value="tn">TN</option>
+                    <option value="va">VA</option>
+                    <option value="va">OLED</option>
+                </select>
+            </div>
+            <div>
+                <label htmlFor="backlight">Backlight Type:</label>
+                <select
+                    id="backlight"
+                    name="backlight"
+                    value={backlight}
+                    onChange={handleInputChange}
+                >
+                    <option value="nopref">No preference</option>
+                    <option value="oled">MiniLED</option>
+                </select>
+            </div>
         </div>
-      )}
-      <div>
-        <label htmlFor="minRR">Minimum Refresh Rate:</label>
-        <select
-          id="minRR"
-          name="minRR"
-          value={minRR}
-          onChange={handleInputChange}
-        >
-          <option value="nopref">No preference</option>
-          <option value="60hz">60Hz</option>
-          <option value="75hz">75Hz</option>
-          <option value="120hz">120Hz</option>
-          <option value="144hz">144Hz</option>
-          <option value="240hz">240Hz</option>
-        </select>
-      </div>
-      <div>
-        <label htmlFor="panel">Panel Type:</label>
-        <select
-          id="panel"
-          name="panel"
-          value={panel}
-          onChange={handleInputChange}
-        >
-          <option value="nopref">No preference</option>
-          <option value="ips">IPS</option>
-          <option value="tn">TN</option>
-          <option value="va">VA</option>
-          <option value="va">OLED</option>
-        </select>
-      </div>
-      <div>
-        <label htmlFor="backlight">Backlight Type:</label>
-        <select
-          id="backlight"
-          name="backlight"
-          value={backlight}
-          onChange={handleInputChange}
-        >
-          <option value="nopref">No preference</option>
-          <option value="oled">MiniLED</option>
-        </select>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default OptionalFilterForm;
--- vite-project/src/Special.tsx
+++ vite-project/src/Special.tsx
@@ -1,67 +1,72 @@
-import React, { useState } from "react";
-import "./Component.css";
+import React, { useState } from 'react';
+import './Component.css';
 
 interface Props {}
 
 const SpecialForm: React.FC<Props> = () => {
-  const [print, setPrint] = useState<string>("no");
-  const [edit, setEdit] = useState<string>("no");
-  const [grade, setGrade] = useState<string>("no");
+    const [print, setPrint] = useState<string>('no');
+    const [edit, setEdit] = useState<string>('no');
+    const [grade, setGrade] = useState<string>('no');
 
-  const handleInputChange = (
-    event: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
-  ) => {
-    const { name, value } = event.target;
-    switch (name) {
-      case "print":
-        setPrint(value);
-        break;
-      case "edit":
-        setEdit(value);
-        break;
-      case "grade":
-        setGrade(value);
-        break;
-      default:
-        break;
-    }
-  };
+    const handleInputChange = (
+        event: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
+    ) => {
+        const { name, value } = event.target;
+        switch (name) {
+            case 'print':
+                setPrint(value);
+                break;
+            case 'edit':
+                setEdit(value);
+                break;
+            case 'grade':
+                setGrade(value);
+                break;
+            default:
+                break;
+        }
+    };
 
-  return (
-    <div className="form-container">
-      <div>
-        <label htmlFor="print">Print:</label>
-        <select
-          id="print"
-          name="print"
-          value={print}
-          onChange={handleInputChange}
-        >
-          <option value="no">No</option>
-          <option value="yes">Yes</option>
-        </select>
-      </div>
-      <div>
-        <label htmlFor="edit">Edit:</label>
-        <select id="edit" name="edit" value={edit} onChange={handleInputChange}>
-          <option value="no">No</option>
-          <option value="yes">Yes</option>
-        </select>
-      </div>
-      <div>
-        <label htmlFor="grade">Grade:</label>
-        <select
-          id="grade"
-          name="grade"
-          value={grade}
-          onChange={handleInputChange}
-        >
-          <option value="no">No</option>
-          <option value="yes">Yes</option>
-        </select>
-      </div>
-    </div>
-  );
+    return (
+        <div className="form-container">
+            <div>
+                <label htmlFor="print">Print:</label>
+                <select
+                    id="print"
+                    name="print"
+                    value={print}
+                    onChange={handleInputChange}
+                >
+                    <option value="no">No</option>
+                    <option value="yes">Yes</option>
+                </select>
+            </div>
+            <div>
+                <label htmlFor="edit">Edit:</label>
+                <select
+                    id="edit"
+                    name="edit"
+                    value={edit}
+                    onChange={handleInputChange}
+                >
+                    <option value="no">No</option>
+                    <option value="yes">Yes</option>
+                </select>
+            </div>
+            <div>
+                <label htmlFor="grade">Grade:</label>
+                <select
+                    id="grade"
+                    name="grade"
+                    value={grade}
+                    onChange={handleInputChange}
+                >
+                    <option value="no">No</option>
+                    <option value="yes">Yes</option>
+                </select>
+            </div>
+        </div>
+    );
 };
 
 export default SpecialForm;
