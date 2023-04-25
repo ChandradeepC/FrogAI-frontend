84eba13896e30d8c331554b3580e71eed9f24dff
Chandradeep Chowdhury <chandradeep.choudhury@gmail.com>
2023-04-24 20:00:36 -0700

    cleaned imports

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,5 +1,4 @@
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
 import frogaiLogo from './assets/frogai.jpg';
 import './App.css';
 import DeviceAndBudgetForm from './DeviceAndBudget';
